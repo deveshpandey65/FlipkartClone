c0a9e5cd157f6b306657931a98425cd4188eea7d
Devesh <dp.devesh65@gmail.com>
2024-04-21 00:00:05 +0530

    worked on products list

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Header from './Header'
 import Options from './Options'
 import ImagesScroll from './ImagesScroll'
+import Productslist from './Productslist'
 
 export default function App() {
   return (
     <>
       <Header />
       <div className='screen'>
-        <br/>
+        
         <Options />
         <br/>
         <ImagesScroll/>
+        <br/>
+        <Productslist/>
+        <br/>
       </div>
     </>
     
---
+++ src/Productslist.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+export default function Productslist() {
+  return (
+    <>
+        <div className='Productlist'>
+            <div className='Productlist-title'>
+                <h4>Best Of Electronics</h4>
+                <div className='nextbtn'>
+                    <p>&gt;</p>
+                </div>
+            </div>
+            <div className='Productlist-des'>
+                <div className='Productlist-des-block'>
+                      <img src="https://rukminim1.flixcart.com/flap/170/170/image/24ed491dc3ff9e8a.jpg?q=80" alt="" />
+                      <p>Noise Smartwatches</p>
+                      <h5>From $1399</h5>
+                </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+                  <div className='Productlist-des-block'>
+
+                  </div>
+
+            </div>
+
+        </div>
+    </>
+  )
+}
