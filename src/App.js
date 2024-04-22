aff6629b397b6b764b7c760c3ce25ad11b1fd8bd
Devesh <dp.devesh65@gmail.com>
2024-04-22 22:12:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,29 +11,29 @@ export default function App() {
       <Header />
       <div className='screen'>
         <Options />
-        <br/>
-        <ImagesScroll/>
-        <br/>
+        <br />
+        <ImagesScroll />
+        <br />
         <Productslist data={'Best Of Electronics'} />
-        
+
         <Productslist data={'Beauty,Food,Toys & more'} />
         <div className='productlistbigblock'>
           <ProductslistBig data={'Summer Decor & Furnishing'} />
           <div className='productlistbigblock-img'>
-            <img  src='https://rukminim1.flixcart.com/www/1070/750/promos/26/09/2023/ed27f892-1bc6-462f-805b-953f5add4f6a.jpg?q=80' />
-       </div>
-            </div>
-        <br/>
+            <img src='https://rukminim1.flixcart.com/www/1070/750/promos/26/09/2023/ed27f892-1bc6-462f-805b-953f5add4f6a.jpg?q=80' />
+          </div>
+        </div>
+        <br />
         <div className='productlistbigblock'>
           <ProductslistBig data={'Shop Student Essentials'} />
           <ProductslistBig data={'Festival Specials, Just For You!'} />
           <ProductslistBig data={'Latest Summer Styles'} />
         </div>
-        <br/>
-        <Productslist data={'Home & Kitchen Essential'}/>
-        <br/>
+        <br />
+        <Productslist data={'Home & Kitchen Essential'} />
+        <br />
       </div>
     </>
-    
+
   )
 }
